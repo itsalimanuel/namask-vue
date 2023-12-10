a1c54e167780519f2fd4a18914b56b6ebfb3e416
itsalimanuel <itskhaloufali@gmail.com>
2023-12-10 15:17:49 +0400

    (chore): close/outside

---
+++ src/components/closeIcon.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = defineProps({
+  cn: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :class="props.cn"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path
+      d="M18 6L6 18"
+      stroke="white"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M6 6L18 18"
+      stroke="white"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
--- src/package/chat.vue
+++ src/package/chat.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
+import { onClickOutside } from "@vueuse/core";
+import logo from '../../public/black-logo.svg'
+import CloseIcon from "../components/closeIcon.vue";
 const isHovered = ref(false);
 const isOpen = ref(false);
+
+const boxRef = ref<HTMLElement | null>(null);
+
 const props = defineProps({
   width: {
     type: Number,
@@ -23,7 +29,12 @@ const props = defineProps({
     type: Number,
     default: 24,
   },
+  icon: {
+    type: String,
+    default: logo
+  }
 });
+onClickOutside(boxRef, () => (isOpen.value = false));
 
 const hexToRgba = (hex: string, alpha: number) => {
   const bigint = parseInt(hex.slice(1), 16);
@@ -53,64 +64,47 @@ const svgStyle = computed(() => ({
 </script>
 
 <template>
-  <button
-    class="-_chat"
-    v-if="!isOpen"
-    :style="styleProps"
-    @mouseover="isHovered = true"
-    @mouseout="isHovered = false"
-    @click="isOpen = true"
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      :style="svgStyle"
-      viewBox="0 0 24 24"
-      fill="none"
+  <div>
+    <button
+      class="-_chat"
+      v-if="!isOpen"
+      :style="styleProps"
+      @mouseover="isHovered = true"
+      @mouseout="isHovered = false"
+      @click="isOpen = true"
     >
-      <path
-        d="M21 15C21 15.5304 20.7893 16.0391 20.4142 16.4142C20.0391 16.7893 19.5304 17 19 17H7L3 21V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H19C19.5304 3 20.0391 3.21071 20.4142 3.58579C20.7893 3.96086 21 4.46957 21 5V15Z"
-        stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
-  </button>
-  <div class="--box" v-else>
-    <div class="--box-header">
-      <div class="--box-header-bg"></div>
-      <div class="--box-header-filter"></div>
-      <div class="--flex">
-        <div class="--flex-header">
-          <div class="--logo"></div>
-          <button @click="isOpen = !isOpen" class="--close">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-            >
-              <path
-                d="M18 6L6 18"
-                stroke="white"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M6 6L18 18"
-                stroke="white"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          </button>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        :style="svgStyle"
+        viewBox="0 0 24 24"
+        fill="none"
+      >
+        <path
+          d="M21 15C21 15.5304 20.7893 16.0391 20.4142 16.4142C20.0391 16.7893 19.5304 17 19 17H7L3 21V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H19C19.5304 3 20.0391 3.21071 20.4142 3.58579C20.7893 3.96086 21 4.46957 21 5V15Z"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </button>
+    <div class="--box" v-else ref="boxRef">
+      <div class="--box-header">
+        <div class="--box-header-bg"></div>
+        <div class="--box-header-filter"></div>
+        <div class="--flex">
+          <div class="--flex-header">
+            <div class="--logo">
+              <img :src="props.icon" alt="">
+            </div>
+            <button @click="isOpen = !isOpen" class="--close">
+             <CloseIcon cn="is-close-icon" />
+            </button>
+          </div>
         </div>
       </div>
+      <div class="--box-body"></div>
     </div>
-    <div class="--box-body"></div>
   </div>
 </template>
 
@@ -198,12 +192,16 @@ const svgStyle = computed(() => ({
   display: flex;
   width: 60px;
   height: 60px;
-  background: #000;
+  background: #fff;
   border-radius: 100%;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 8px;
+}
+.--logo img {
+  width: 30px;
+  height: 30px;
 }
 .--close {
   border-radius: 100%;
@@ -223,6 +221,12 @@ const svgStyle = computed(() => ({
   gap: 35px;
   align-self: stretch;
   background: #fff;
-  height: 300px;
+  height: 400px;
+}
+.is-close-icon {
+  width: 24px;
+  height: 24px;
+  min-width: 24px;
+  min-height: 24px;
 }
 </style>
